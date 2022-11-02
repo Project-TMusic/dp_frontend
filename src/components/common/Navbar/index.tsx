533de304dfa989d92202a35af07d37c5bd87892d
alpha0212 <qkrcjfgml321@naver.com>
2022-11-02 15:54:22 +0900

    Setting Position

---
+++ src/components/common/Navbar/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import * as S from './styled';
+
+export const Navbar: React.FC = () => {
+  return (
+    <S.NavbarContainer>
+      <S.LogoContainer>
+        <S.Logo>
+          <S.LogoText>dp</S.LogoText>
+        </S.Logo>
+      </S.LogoContainer>
+      <S.MenuContainer>
+        <S.MenusLinkContainer>
+          <S.Home>Home</S.Home>
+          <S.MenuLink>카테고리</S.MenuLink>
+          <S.MenuLink>상품</S.MenuLink>
+          <S.MenuLink>About us</S.MenuLink>
+        </S.MenusLinkContainer>
+        <S.ProfileContainer>
+          <S.Wish></S.Wish>
+          <S.Basket></S.Basket>
+          <S.HelloUser>Hello, User</S.HelloUser>
+          <S.Profile></S.Profile>
+        </S.ProfileContainer>
+      </S.MenuContainer>
+    </S.NavbarContainer>
+  );
+};
