4e4f23b2cc9524a45c145926814e516da2089057
alpha0212 <qkrcjfgml321@naver.com>
2022-11-02 15:55:55 +0900

    Fix Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Routes, Route } from 'react-router';
 import { Main } from 'src/pages';
+import { Navbar } from 'src/components';
 export const App: React.FC = () => (
   <Routes>
-    <Route index element={<Main />}></Route>
+    <Navbar />
+    <Route index element={<Main />} />
   </Routes>
 );
