75e611a2022e6a98f85c751780b8e6eea29ac94d
[1O16] <youiiyou1253@gmail.com>
2022-11-02 23:53:26 +0900

    About Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router';
-import { Main } from 'src/pages';
+import { About, Main } from 'src/pages';
 import { Navbar } from 'src/components';
 export const App: React.FC = () => (
   <>
     <Navbar />
     <Routes>
       <Route index element={<Main />} />
+      <Route path="about" element={<About />} />
     </Routes>
   </>
 );
--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import * as S from './styled';
 
@@ -15,7 +16,9 @@ export const Navbar: React.FC = () => {
           <S.Home>Home</S.Home>
           <S.MenuLink>카테고리</S.MenuLink>
           <S.MenuLink>상품</S.MenuLink>
-          <S.MenuLink>About us</S.MenuLink>
+          <S.MenuLink>
+            <Link to="about">About us</Link>
+          </S.MenuLink>
         </S.MenusLinkContainer>
         <S.ProfileContainer>
           <S.Wish></S.Wish>
