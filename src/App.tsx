03fcffaf795f1346cf6c13e534a4fb22b610d46a
alpha0212 <qkrcjfgml321@naver.com>
2022-11-09 00:24:53 +0900

    나머지 커밋

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router';
-import { About, Main, ProfilePage } from 'src/pages';
+import { About, Main, Profile } from 'src/pages';
 import { Navbar } from 'src/components';
 import { Global } from '@emotion/react';
 import { globalPadding } from './styles/globalPadding';
@@ -10,9 +10,11 @@ export const App: React.FC = () => (
     <Global styles={globalPadding} />
     <Navbar />
     <Routes>
-      <Route index element={<Main />} />
+      <Route path="/" element={<Main />}>
+        <Route path="food" element={<Main />} />
+      </Route>
       <Route path="about" element={<About />} />
-      <Route path="profile" element={<ProfilePage />} />
+      <Route path="profile" element={<Profile />} />
     </Routes>
   </>
 );
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,3 +2,4 @@ export * from './Navbar';
 export * from './Banner';
 export * from './AboutUs';
 export * from './Category';
+export * from './Products';
