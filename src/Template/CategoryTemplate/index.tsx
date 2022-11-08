45b3261647aae2a834c71cdadd900affaa011bbe
alpha0212 <qkrcjfgml321@naver.com>
2022-11-09 00:15:28 +0900

    Feature: Category ReWrite

--- src/Template/CategoryTemplate/index.tsx
+++ src/Template/CategoryTemplate/index.tsx
@@ -1,16 +1,15 @@
 import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import { Food, Clothes } from 'src/components/common/Products/List';
 import { Category } from 'src/components';
 
 export const CategoryTemplate: React.FC = () => {
   const location = useLocation();
-  const categoryType = location.pathname.split('/')[5];
   return (
     <>
       <Category
-        CategoryTemplateType={categoryType as any}
-        food={<></>}
-        clothes={<></>}
+        food={<Food />}
+        clothes={<Clothes />}
         health={<></>}
         computer={<></>}
         tool={<></>}
