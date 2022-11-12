6c7b814c18418b85728ac9195db99b037e1963aa
alpha0212 <qkrcjfgml321@naver.com>
2022-11-13 02:12:28 +0900

    Add: Card

--- src/components/common/Products/List/Food/index.tsx
+++ src/components/common/Products/List/Food/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import * as S from './styled';
+import { Card, Empty } from 'src/components';
 
 export const Food: React.FC = () => {
-  return <div>짜라란~</div>;
+  return (
+    <>
+      <Card cardtitleprops="aasd" priceprops={1000} imgprops="asd" />
+      <Empty />
+      <Card cardtitleprops="aasd" priceprops={1000} imgprops="asd" />
+      <Empty />
+      <Card cardtitleprops="aasd" priceprops={1000} imgprops="asd" />
+    </>
+  );
 };
