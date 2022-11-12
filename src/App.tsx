f052c0fc757a62e4ac3641d4108404aa3c7d0884
[1O16] <youiiyou1253@gmail.com>
2022-11-13 02:59:51 +0900

    Register and Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router';
-import { About, Main, ProfilePage } from 'src/pages';
+import { About, Login, Main, ProfilePage, Register } from 'src/pages';
 import { Navbar } from 'src/components';
 import { Global } from '@emotion/react';
 import { globalPadding } from './styles/globalPadding';
@@ -15,6 +15,8 @@ export const App: React.FC = () => (
       <Route path="clothes" element={<Main />} />
       <Route path="about" element={<About />} />
       <Route path="profile" element={<ProfilePage />} />
+      <Route path="register" element={<Register />} />
+      <Route path="login" element={<Login />} />
     </Routes>
   </>
 );
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,5 @@
 export * from './Main';
 export * from './About';
 export * from './Profile';
+export * from './Account/Login';
+export * from './Account/Register';
