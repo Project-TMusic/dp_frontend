c3839fe313e55e5953222bdf064921acc4c54d57
alpha0212 <qkrcjfgml321@naver.com>
2022-11-20 19:32:00 +0900

    Feature: Footer for Main

---
+++ src/components/common/Footer/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import * as S from './styled';
+
+export const Footer: React.FC = () => {
+  return (
+    <S.FooterContainer>
+      <S.FooterLogoContainer>
+        <S.FooterLogo></S.FooterLogo>
+      </S.FooterLogoContainer>
+      <S.coverMargin>
+        <S.AboutContainer>
+          <S.About>About</S.About>
+          <S.FooterText>회사소개</S.FooterText>
+          <S.FooterText>사용방법</S.FooterText>
+          <S.FooterText>팀원</S.FooterText>
+        </S.AboutContainer>
+        <S.CompanyInfoContainer>
+          <S.CompanyInfo>Info</S.CompanyInfo>
+          <S.FooterText>
+            © 2022. Drone Package Co. all rights reserved.
+          </S.FooterText>
+        </S.CompanyInfoContainer>
+      </S.coverMargin>
+    </S.FooterContainer>
+  );
+};
---
+++ src/components/common/Footer/styled.ts
@@ -0,0 +1,52 @@
+import styled from '@emotion/styled';
+
+export const FooterContainer = styled.div`
+  display: flex;
+  background: rgba(0, 0, 0, 0.8);
+  padding-top: 6rem;
+`;
+
+export const coverMargin = styled.div`
+  width: 100%;
+  padding-bottom: 8rem;
+  display: flex;
+`;
+
+export const AboutContainer = styled.div`
+  margin-left: 40%;
+`;
+
+export const About = styled.div`
+  font-family: 'Pretendard';
+  font-size: 3rem;
+  font-weight: 600;
+  color: rgba(255, 255, 255, 0.8);
+`;
+
+export const CompanyInfoContainer = styled.div`
+  margin-left: 14%;
+`;
+
+export const CompanyInfo = styled.div`
+  font-family: 'Pretendard';
+  font-size: 3rem;
+  font-weight: bold;
+  color: rgba(255, 255, 255, 0.8);
+`;
+
+export const FooterText = styled.div`
+  font-size: 1.5rem;
+  font-weight: 300;
+  color: rgba(255, 255, 255, 0.3);
+  margin-top: 2rem;
+`;
+
+export const FooterLogoContainer = styled.div``;
+
+export const FooterLogo = styled.div`
+  width: 30rem;
+  padding-top: 15rem;
+  margin-top: 1rem;
+  margin-left: 5rem;
+  background: gray;
+`;
