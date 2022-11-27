5fb92b2e78116ffc53614606816fe95c07362f7c
alpha0212 <qkrcjfgml321@naver.com>
2022-11-27 20:59:37 +0900

    style

---
+++ src/components/common/Payment/MainSection/ThreeLine/styled.ts
@@ -0,0 +1,50 @@
+import styled from '@emotion/styled';
+
+export const ThreeLineContainer = styled.div`
+  display: flex;
+  width: 60rem;
+  height: 10rem;
+  margin-top: 5rem;
+`;
+
+export const ThreeLineTitle = styled.div`
+  font-size: 2rem;
+  font-weight: bold;
+`;
+
+export const ThreeLineHelp = styled.div`
+  color: rgba(0, 0, 0, 0.3);
+  font-size: 1.2rem;
+  font-weight: bold;
+  margin-top: 2rem;
+`;
+
+export const ThreeLineCVCContainer = styled.div`
+  display: flex;
+  width: 30rem;
+  height: 7rem;
+  background: rgba(243, 243, 243, 0.5);
+  border-radius: 1.5rem;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  margin-left: 10rem;
+`;
+
+export const ThreeLineCVC = styled.input`
+  border: none;
+  outline: none;
+  background: none;
+  text-align: center;
+  font-size: 2rem;
+  font-weight: 500;
+  width: 10rem;
+  height: 5rem;
+  margin-top: 1rem;
+  margin: auto;
+`;
+
+export const ThreeLineLeft = styled.div`
+  width: 20rem;
+  margin-top: 1rem;
+`;
+
+export const ThreeLineRight = styled.div``;
