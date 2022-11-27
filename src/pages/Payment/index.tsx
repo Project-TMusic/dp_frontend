8f8ab6e4788c159d2e74f83767b7e0e1598d871c
alpha0212 <qkrcjfgml321@naver.com>
2022-11-27 20:58:00 +0900

    Feature: Data Setting

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -1,6 +1,55 @@
-import React from 'react';
-import { Payment } from 'src/components/common/Payment';
+import React, { useState } from 'react';
+import {
+  EmptyCircle,
+  IcCard,
+  Receipt,
+  ThreeLine,
+  TwoLine,
+} from 'src/components';
+import { OneLine } from 'src/components/common/Payment/MainSection/OneLine';
+
+import * as S from './styled';
 
 export const PaymentPage: React.FC = () => {
-  return <Payment />;
+  const [oneNumber, setOneNumber] = useState('');
+  const [twoNumber, setTwoNumber] = useState('');
+  const [threeNumber, setThreeNumber] = useState('');
+  const [fourNumber, setFourNumber] = useState('');
+
+  let check = false;
+  if (
+    oneNumber.length == 4 &&
+    twoNumber.length == 4 &&
+    threeNumber.length == 4 &&
+    fourNumber.length == 4
+  ) {
+    check = true;
+  }
+  return (
+    <>
+      <S.CardContent>
+        <S.MainSection>
+          <OneLine />
+          <TwoLine
+            check={check}
+            setOneNumber={setOneNumber}
+            setTwoNumber={setTwoNumber}
+            setThreeNumber={setThreeNumber}
+            setFourNumber={setFourNumber}
+          />
+          <ThreeLine />
+        </S.MainSection>
+        <S.Paypaper>
+          <Receipt />
+          <IcCard
+            OneNumber={oneNumber}
+            TwoNumber={twoNumber}
+            ThreeNumber={threeNumber}
+            FourNumber={fourNumber}
+          />
+          <EmptyCircle />
+        </S.Paypaper>
+      </S.CardContent>
+    </>
+  );
 };
---
+++ src/pages/Payment/styled.ts
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const CardContent = styled.div`
+  display: flex;
+  margin-left: 5rem;
+  margin-top: 3rem;
+`;
+
+export const Paypaper = styled.div`
+  width: 55rem;
+`;
+
+export const MainSection = styled.div`
+  width: 90%;
+  margin-left: 5%;
+`;
