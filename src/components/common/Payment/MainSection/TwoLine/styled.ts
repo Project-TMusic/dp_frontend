c863cc49e6f01e9e380ce9385536ff0e85a3d262
alpha0212 <qkrcjfgml321@naver.com>
2022-11-27 20:58:56 +0900

    style

---
+++ src/components/common/Payment/MainSection/TwoLine/styled.ts
@@ -0,0 +1,50 @@
+import styled from '@emotion/styled';
+
+export const TwoLineContainer = styled.div`
+  margin-top: 5.5rem;
+`;
+
+export const TwoLineTitle = styled.div`
+  font-size: 2.2rem;
+  font-weight: bold;
+`;
+
+export const TwoLineHelp = styled.div`
+  color: rgba(0, 0, 0, 0.3);
+  font-size: 1.2rem;
+  font-weight: bold;
+  margin-top: 2rem;
+`;
+
+export const TwoLineInputCardNumberContainer = styled.div`
+  display: flex;
+  width: 60rem;
+  height: 7rem;
+  background: rgba(243, 243, 243, 0.5);
+
+  border-radius: 1.5rem;
+  margin-top: 4rem;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+`;
+
+export const TwoLineInputCardNumber = styled.input`
+  border: none;
+  outline: none;
+  background: none;
+  text-align: center;
+  font-size: 1.5rem;
+  font-weight: 500;
+  width: 10rem;
+  height: 5rem;
+  margin-top: 1rem;
+  margin-right: 0.3rem;
+`;
+
+export const TwoLineInputCardSet = styled.div``;
+
+export const TwoLineCheckImg = styled.img`
+  width: 2rem;
+  height: 2rem;
+  margin-top: 2.5rem;
+  margin-left: 4rem;
+`;
