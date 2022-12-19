53ffc9827885b3b2eee6f44fac09694c18ae8204
alpha0212 <qkrcjfgml321@naver.com>
2022-12-19 15:17:59 +0900

    Feat: Add API

--- src/api/product/api.ts
+++ src/api/product/api.ts
@@ -1,4 +1,4 @@
-export const DUMMY_ITEM = [
+export const DUMMY_ITEM_Food = [
   {
     id: 1 as number,
     name: '엽기떡볶이' as string,
@@ -16,6 +16,30 @@ export const DUMMY_ITEM = [
   {
     id: 1 as number,
     name: '스프링초밥' as string,
+    packingState: '포장불가' as string,
+    description: '',
+    img: 'https://thumb.mt.co.kr/06/2021/03/2021031910212950541_1.jpg/dims/optimize/' as string,
+  },
+];
+
+export const DUMMY_ITEM_Clothes = [
+  {
+    id: 1 as number,
+    name: '동대문할매옷가게' as string,
+    packingState: '포장불가' as string,
+    description: '',
+    img: 'https://www.yupdduk.com/images/logo61.png' as string,
+  },
+  {
+    id: 2 as number,
+    name: '너좀입니' as string,
+    packingState: '포장불가' as string,
+    description: '',
+    img: 'http://baedduck.co.kr/theme/bae-default/layout/assets/img/logo-b.png',
+  },
+  {
+    id: 1 as number,
+    name: '스프링옷' as string,
     packingState: '포장불가' as string,
     description: '',
     img: 'https://thumb.mt.co.kr/06/2021/03/2021031910212950541_1.jpg/dims/optimize/' as string,
