166985fca41e39696cacb7a761635f8d15fd6ef3
alpha0212 <qkrcjfgml321@naver.com>
2022-12-19 23:43:33 +0900

    Recoil Example

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -1,6 +1,10 @@
 import { atom } from 'recoil';
+import { ITodo } from 'src/types/interfaces';
 
-export const todoAtom = atom<string>({
-  default: '',
+export const todoAtom = atom<ITodo>({
+  default: {
+    name: '박준희',
+    address: '포스빌1113호',
+  },
   key: 'Address',
 });
--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import * as S from './styled';
 import { useRecoilState } from 'recoil';
 import { todoAtom } from 'src/atoms';
+import { ITodo } from 'src/types/interfaces';
 
 export const TestPage: React.FC = () => {
   const [sticky, setSticky] = useState(0);
@@ -16,14 +17,17 @@ export const TestPage: React.FC = () => {
       window.addEventListener('scroll', listenScrollEvent);
     };
   }, []);
-  const [address, setAddress] = useRecoilState(todoAtom);
+  const [address, setAddress] = useRecoilState<ITodo>(todoAtom);
   return (
     <S.heightPadding>
       <S.container>
         <S.sidebar>
           <S.stick style={{ opacity: sticky }}>엉</S.stick>
         </S.sidebar>
-        <S.longtext>{address}</S.longtext>
+        <S.longtext>
+          {address.name}
+          {address.address}
+        </S.longtext>
       </S.container>
     </S.heightPadding>
   );
