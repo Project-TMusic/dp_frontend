8ada5ef09f8ec174e0f0b3a4dce79d75711323c3
[1O16] <youiiyou1253@gmail.com>
2022-12-23 02:34:18 +0900

    Food Product 추가

--- src/api/product/api.ts
+++ src/api/product/api.ts
@@ -22,20 +22,41 @@ export const All_Product = [
   },
   {
     id: 4 as number,
+    name: '모래내설농탕' as string,
+    packingState: '포장불가' as string,
+    description: '',
+    img: 'https://blog.kakaocdn.net/dn/cBD7qa/btqS3a9OwhR/HmgwBqVJEQhOKzRj3pWKM0/img.jpg' as string,
+  },
+  {
+    id: 5 as number,
+    name: '모래내곱창' as string,
+    packingState: '포장불가' as string,
+    description: '',
+    img: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjVfMTQ4%2FMDAxNjYxMzk1NDUwMjcx.xoa-6nNgK-O4rbJZgPJfhUcXAJ2IRmv_Vr_-_SL6NoYg.y-joOyYLxTfkUi4Ypvc0kXpc844zVw9viwKOSfswg0sg.JPEG.dingo_o%2FIMG_9515.JPG' as string,
+  },
+  {
+    id: 6 as number,
+    name: '돼지게티' as string,
+    packingState: '포장불가' as string,
+    description: '',
+    img: 'https://d3af5evjz6cdzs.cloudfront.net/images/uploads/320x0/porkghetti-paengseong-logo_056dc69f50186323a31d7559ea9b8c381646715489.jpg' as string,
+  },
+  {
+    id: 7 as number,
     name: '동대문할매옷가게' as string,
     packingState: '포장불가' as string,
     description: '',
     img: 'https://www.yupdduk.com/images/logo61.png' as string,
   },
   {
-    id: 5 as number,
+    id: 8 as number,
     name: '너좀입니' as string,
     packingState: '포장불가' as string,
     description: '',
     img: 'http://baedduck.co.kr/theme/bae-default/layout/assets/img/logo-b.png',
   },
   {
-    id: 6 as number,
+    id: 9 as number,
     name: '스프링옷' as string,
     packingState: '포장불가' as string,
     description: '',
@@ -65,6 +86,27 @@ export const DUMMY_ITEM_Food = [
     description: '',
     img: 'https://thumb.mt.co.kr/06/2021/03/2021031910212950541_1.jpg/dims/optimize/' as string,
   },
+  {
+    id: 4 as number,
+    name: '모래내설농탕' as string,
+    packingState: '포장불가' as string,
+    description: '',
+    img: 'https://blog.kakaocdn.net/dn/cBD7qa/btqS3a9OwhR/HmgwBqVJEQhOKzRj3pWKM0/img.jpg' as string,
+  },
+  {
+    id: 5 as number,
+    name: '모래내곱창' as string,
+    packingState: '포장불가' as string,
+    description: '',
+    img: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjVfMTQ4%2FMDAxNjYxMzk1NDUwMjcx.xoa-6nNgK-O4rbJZgPJfhUcXAJ2IRmv_Vr_-_SL6NoYg.y-joOyYLxTfkUi4Ypvc0kXpc844zVw9viwKOSfswg0sg.JPEG.dingo_o%2FIMG_9515.JPG' as string,
+  },
+  {
+    id: 6 as number,
+    name: '돼지게티' as string,
+    packingState: '포장불가' as string,
+    description: '',
+    img: 'https://d3af5evjz6cdzs.cloudfront.net/images/uploads/320x0/porkghetti-paengseong-logo_056dc69f50186323a31d7559ea9b8c381646715489.jpg' as string,
+  },
 ];
 
 export const DUMMY_ITEM_Clothes = [
@@ -90,3 +132,5 @@ export const DUMMY_ITEM_Clothes = [
     img: 'https://thumb.mt.co.kr/06/2021/03/2021031910212950541_1.jpg/dims/optimize/' as string,
   },
 ];
+
+export const DUMMY_ITEM_HEALTH = {};
