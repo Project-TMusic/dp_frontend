5c63f58cbf75155745945a0a9c1fbda2bde835b8
alpha0212 <qkrcjfgml321@naver.com>
2022-12-25 02:03:27 +0900

    Header test

---
+++ src/pages/test2/index.tsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect, useRef } from 'react';
+import styled from '@emotion/styled';
+import useScrollDirection from 'src/hooks/useScrollDirection';
+import * as S from './styled';
+
+export const TestPage2: React.FC = () => {
+  const [position, setPosition] = useState(window.pageYOffset);
+  const [visible, setVisible] = useState(true);
+  useEffect(() => {
+    const handleScroll = () => {
+      const moving = window.pageYOffset;
+
+      setVisible(position > moving);
+      setPosition(moving);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.addEventListener('scroll', handleScroll);
+    };
+  });
+
+  const cls = visible ? true : false;
+  return (
+    <div>
+      <S.Header ScrollBoolean={cls}></S.Header>
+      <S.asd>asd</S.asd>
+      <S.asd>asd</S.asd>
+      <S.asd>asd</S.asd>
+    </div>
+  );
+};
