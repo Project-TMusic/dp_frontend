63a47a08825f3bf73f1f4b860d1fe4d6b964852e
alpha0212 <qkrcjfgml321@naver.com>
2022-12-27 23:11:57 +0900

    About: Vision

---
+++ src/components/common/AboutComponents/AboutVision/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import * as S from './styled';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+export const AboutVision: React.FC = () => {
+  return <S.AboutCardContainer></S.AboutCardContainer>;
+};
