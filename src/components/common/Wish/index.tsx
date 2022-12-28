4e4bc3596c7648f7a0f7b8e0339e13d7a0a3a723
alpha0212 <qkrcjfgml321@naver.com>
2022-12-28 14:47:27 +0900

    Feat Wish: Select Category

--- src/components/common/Wish/index.tsx
+++ src/components/common/Wish/index.tsx
@@ -1,7 +1,15 @@
 import React, { useRef, useState } from 'react';
 import * as S from './styled';
+import { All_Product } from 'src/api';
 
 export const Wish: React.FC = () => {
+  const [CategoryProduct, setCategoryProduct] = useState('');
+  const ChangeCategoryFood = () => {
+    setCategoryProduct('food');
+  };
+  const ChangeCategoryClothes = () => {
+    setCategoryProduct('clothes');
+  };
   return (
     <S.WishAllContainer>
       <S.positionCenter>
@@ -13,60 +21,34 @@ export const Wish: React.FC = () => {
         </S.WishHeaderContainer>
         <>
           <S.DropdownUnorderdList>
-            <S.DropdownListItem>Food</S.DropdownListItem>
-            <S.DropdownListItem>Clothes</S.DropdownListItem>
+            <S.DropdownListItem onClick={ChangeCategoryFood}>
+              Food
+            </S.DropdownListItem>
+            <S.DropdownListItem onClick={ChangeCategoryClothes}>
+              Clothes
+            </S.DropdownListItem>
             <S.DropdownListItem>Health</S.DropdownListItem>
             <S.DropdownListItem>Tool</S.DropdownListItem>
           </S.DropdownUnorderdList>
         </>
         <S.WishCenterDisplay>
           <S.WishListContainer>
-            <S.WishListContent>
-              <S.WishListImage />
-              <S.WishListDescription>
-                <S.WishListTitle>남가좌동점</S.WishListTitle>
-                <S.WishListHeart>❤️</S.WishListHeart>
-              </S.WishListDescription>
-              <S.WishListStore>
-                가게 설명 가게 설명 가게 설명 가게 설명 가게 설명 가게 설명 가게
-                설명 가게 설명 가게 설명
-              </S.WishListStore>
-            </S.WishListContent>
-            <S.EmptySpace />
-            <S.WishListContent>
-              <S.WishListImage />
-              <S.WishListDescription>
-                <S.WishListTitle></S.WishListTitle>
-                <S.WishListHeart>❤️</S.WishListHeart>
-              </S.WishListDescription>
-              <S.WishListStore>
-                가게 설명 가게 설명 가게 설명 가게 설명 가게 설명 가게 설명 가게
-                설명 가게 설명 가게 설명
-              </S.WishListStore>
-            </S.WishListContent>
-            <S.EmptySpace />
-            <S.WishListContent>
-              <S.WishListImage />
-              <S.WishListDescription>
-                <S.WishListTitle></S.WishListTitle>
-                <S.WishListHeart>❤️</S.WishListHeart>
-              </S.WishListDescription>
-              <S.WishListStore>
-                가게 설명 가게 설명 가게 설명 가게 설명 가게 설명 가게 설명 가게
-                설명 가게 설명 가게 설명
-              </S.WishListStore>
-            </S.WishListContent>
-            <S.WishListContent>
-              <S.WishListImage></S.WishListImage>
-              <S.WishListDescription>
-                <S.WishListTitle>준희네 떡볶이 남가좌동점</S.WishListTitle>
-                <S.WishListHeart>❤️</S.WishListHeart>
-              </S.WishListDescription>
-              <S.WishListStore>
-                가게 설명 가게 설명 가게 설명 가게 설명 가게 설명 가게 설명 가게
-                설명 가게 설명 가게 설명
-              </S.WishListStore>
-            </S.WishListContent>
+            {All_Product.filter((value) => {
+              if (CategoryProduct === value.tag) {
+                return value;
+              }
+            }).map((value: any, key: any) => {
+              return (
+                <S.WishListContent key={key}>
+                  <S.WishListImage />
+                  <S.WishListDescription>
+                    <S.WishListTitle>{value.name}</S.WishListTitle>
+                    <S.WishListHeart>❤️</S.WishListHeart>
+                  </S.WishListDescription>
+                  <S.WishListStore></S.WishListStore>
+                </S.WishListContent>
+              );
+            })}
           </S.WishListContainer>
         </S.WishCenterDisplay>
       </S.positionCenter>
