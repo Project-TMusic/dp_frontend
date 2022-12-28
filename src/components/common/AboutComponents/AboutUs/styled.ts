677032d7e77b852eb296622b12b0e6d4f3d24105
alpha0212 <qkrcjfgml321@naver.com>
2022-12-28 14:03:57 +0900

    AboutUs

--- src/components/common/AboutComponents/AboutUs/styled.ts
+++ src/components/common/AboutComponents/AboutUs/styled.ts
@@ -2,11 +2,12 @@ import styled from '@emotion/styled';
 
 export const AboutUsContainer = styled.div`
   width: 100%;
-  margin-top: 5rem;
+  margin-top: 16rem;
 `;
 
 export const Title = styled.div`
   text-align: center;
   font-size: 9rem;
   font-weight: bold;
+  color: #d0d0d0;
 `;
