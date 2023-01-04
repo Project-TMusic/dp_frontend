23fef6da8d9b727bfc4654d35b9084b6e22530f4
alpha0212 <qkrcjfgml321@naver.com>
2023-01-04 18:06:38 +0900

    reducers

---
+++ src/reducers/CartReducer.ts
@@ -0,0 +1,39 @@
+import { INIT_CART } from 'src/status';
+import { AddToCart, RemoveFromCart } from 'src/action';
+
+export const CartReducer = (state = INIT_CART, action: any) => {
+  switch (action.type) {
+    case AddToCart:
+      // eslint-disable-next-line no-case-declarations
+      const { id, product } = action.payload;
+      console.log(action.payload.id, '반갑워');
+      return {
+        ...state,
+        list: [
+          ...state.list,
+          {
+            id: id,
+            product: product,
+          },
+        ],
+      };
+    case RemoveFromCart:
+      // eslint-disable-next-line no-case-declarations
+      const removeItem = state.list.filter(
+        (elem: any) => elem.id !== action.id
+      );
+      console.log(
+        state.list
+          .filter((elem: any) => elem.id !== action.id)
+          .map((value: any) => value.id),
+        '리므부'
+      );
+      return {
+        ...state,
+        list: removeItem,
+      };
+
+    default:
+      return state;
+  }
+};
